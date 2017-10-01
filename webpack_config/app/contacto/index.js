3913c9ed7fa49b5b047e5b336035a6f6acf6af7b
Carlos Herrera <jcarloshm_93@yahoo.com>
2017-10-01 13:23:34 -0600

    Estilo del formulario css-Juan Carlos Herrera

--- webpack_config/app/contacto/index.js
+++ webpack_config/app/contacto/index.js
@@ -1,3 +1,28 @@
-const contacto = `<h1>contacto</h1>`
+const contacto = `
+
+<html>
+<head>
+	<title>Formulario De Contacto</title>
+	<meta charset="UTF-8">
+	<style type="text/css">
+	body{
+	background-image: url(CONTACTENOS.png);
+	background-size: 100vw 137vh;
+	background-attachment: fixed;
+	margin: 0;
+	font-family: monospace;
+}
+
+form{
+	width: 450px;
+	margin: auto;
+	background: rgba(0,0,0,0.4);
+	box-sizing: border-box;
+	margin-top: 20px;
+	border-radius: 7px;
+}
+
+
+<h1>contacto</h1>`
 
 export default contacto
