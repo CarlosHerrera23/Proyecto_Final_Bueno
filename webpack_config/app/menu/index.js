3c7bb1541fd8a4fee58b3b458bd1ff1eb41a0cee
Carlos Herrera <jcarloshm_93@yahoo.com>
2017-10-14 19:50:24 -0600

    aqui el menu pricipal-Juan Carlos la BD-Angel mayo

--- webpack_config/app/menu/index.js
+++ webpack_config/app/menu/index.js
@@ -1,33 +1,105 @@
- 
+
 
 const menu = `
-<style type="text/css"> 
-#button { 
-padding: 0; 
-} 
-#button li { 
-display: inline; 
-} 
-#button li a { 
-font-family: Arial; 
-font-size:22px; 
-text-decoration: none; 
-float:center; 
-padding: 20px; 
-background-color: #2175bc; 
-color: #fff; 
-} 
-#button li a:hover { 
-background-color: #2586d7; 
-margin-top:-2px; 
-padding-bottom:12px; 
-} 
-</style>
-<ul id ="button">
-	<li><a href="/">Inicio</a></li>
-	<li><a href="/acercade">Acerca de</a></li>
-	<li><a href="/contacto">Contacto</a></li>
-	<li><a href="/categoria">Categoria</a></li>
-</ul>
+<html>
+<head>
+	<title>Inicio</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1 maximum-scale=1, minimun-scale=1">
+	<link rel="stylesheet" type="text/css" href="css/fontello.css">
+	<link rel="stylesheet" type="text/css" href="css/estilos.css">
+</head>
+
+<body>
+	<header>
+		<div class="contenedor">
+			<h1 class="icon-grocery-store">TechMarts</h1>
+			<input type="checkbox" id="menu-bar">
+			<label class="icon-th" for="menu-bar"></label>
+			<nav class="menu">
+				<a href="/">Inicio</a>
+				<a href="/acercade">acerda de</a>
+				<a href="/contacto">Contactos</a>
+				<a href="/categoria">Categoria</a>
+			</nav>
+		</div>
+	</header>
+
+	<main>
+		<section id="banner">
+			<img src="img/tienda.jpg">
+			<div class="contenedor">
+				<h2>TechMarts La Tienda del Futuro</h2>
+				<p>Quieres Conocer mas Hacerca de La tienda</p>
+				<a href="#">Leer Mas</a>
+			</div>
+		</section>
+
+		<section id="bienvenido">
+			<h2>BIENVENIDO A NUESTRA TIENDA EN LINEA </h2>
+			<p>Somos una nueva tienda online. Ya sabemos que hay muchas, y seguramente muchas de ellas mejores que nosotros, pero estamos trabajando muy duro para llegar ahí. Somos un equipo joven y dinámico, a quienes nos encanta lo que hacemos. Intentamos dar a nuestros clientes el mejor servicio, y nos sentimos orgullosos de poco a poco haber ido fidelizado a varios de nuestros primeros clientes. Ofrecemos atención telefónica para cualquier duda, recomendaciones y si buscas algo que no tenemos, nosotros lo buscamos por ti, para aconsejarte donde encontrarlo si no te lo pudiéramos proporcionar nosotros. </p>
+			
+		</section>
+
+		<section id="blog">
+			<h3>Lo ultimo de Nuestra Tienda</h3>
+			<div class="contenedor">
+				<article>
+					<img src="img/iphone8.jpg">
+					<h4>Novedades del Nuevo Iphone</h4>
+				</article>
+				<article>
+					<img src="img/Intel.jpg">
+					<h4>Nuevo Procesador Core I9 Extreme Edition</h4>
+				</article>
+				<article>
+					<img src="img/and.png" >
+					<h4>Novedades De Android</h4>
+				</article>
+
+			</div>
+		</section>
+
+		<section id="info">
+			<h3>Para Nosotros es importante brindarle Productos de calidad y Con garantia A nuestro cliente</h3>
+			<div class="contenedor">
+				<div class="info-pet">
+					<img src="img/certificacion.png">
+					<h4>Certififcados</h4>
+				</div>
+				<div class="info-pet">
+					<img src="img/calidad.png">
+					<h4>Calidad</h4>
+				</div>
+				<div class="info-pet">
+					<img src="img/garantia.png">
+					<h4>Garantia</h4>
+				</div>
+				<div class="info-pet">
+					<img src="img/rembolso.jpg">
+					<h4>Reembolsoo</h4>
+				</div>
+			</div>
+		</section>
+	</main>
+
+	<footer>
+		<div class="contenedor">
+			<p class="copy">TechMarts &copy; 2017</p>
+			<div class="sociales">
+				<a class="icon-facebook-squared" href="#"></a>
+				<a class="icon-twitter" href="#"></a>
+				<a class="icon-instagram" href="#"></a>
+				<a class="icon-youtube-play" href="#"></a>
+				<a class="icon-github" href="#"></a>
+				<a class="icon-gplus" href="#"></a>
+				<a class="icon-twitch" href="#"></a>
+			</div>
+		</div>
+	</footer>
+
+</body>
+</body>
+</html>
 `
 export default menu
