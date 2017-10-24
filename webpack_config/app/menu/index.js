f972a90201efbe91cfcf79b38cea0e27ae383102
Christopher Calero <ccalero@github.com>
2017-10-23 20:40:06 -0600

    Se modifica el menu para que se esconda y aparesca al pasar el cursor del mouse (Chris)

--- webpack_config/app/menu/index.js
+++ webpack_config/app/menu/index.js
@@ -1,13 +1,51 @@
 
 const menu = `
+
+<style type="text/css"> 
+#menu {
+margin: 0; 
+padding: 0; 
+} 
+#menu li { 
+display: inline; 
+} 
+#menu li a { 
+font-family: Arial; 
+font-size:15px; 
+text-decoration: none; 
+float:center; 
+padding: 10px; 
+background-color: none; 
+color: #fff; 
+width: 480px;
+margin: 0 auto;
+} 
+ul{
+	list-style:none
+}
+#menu li a:hover { 
+background-color: #2586d7; 
+margin-top:-7px; 
+padding-bottom:px;
+} 
+#menu li>a:hover{
+  color: black;
+  background-color: #eaeaea;
+}
+#menu>li{
+	text-align:center;
+	display: inline;
+}
+#menu>li>ul{
+  display: none;
+}
+#menu>li:hover>ul {
+  display:block;
+}
+</style>
+
 <html>
-<head>
-	<title>Inicio</title>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1 maximum-scale=1, minimun-scale=1">
-	<link rel="stylesheet" type="text/css" href="css/fontello.css">
-	<link rel="stylesheet" type="text/css" href="css/estilos.css">
-</head>
+
 
 <body>
 	<header>
@@ -15,18 +53,19 @@ const menu = `
 			<h1 class="icon-grocery-store">TechMarts</h1>
 			<input type="checkbox" id="menu-bar">
 			<label class="icon-th" for="menu-bar"></label>
-			<nav class="menu">
-				<a href="/">Inicio</a>
-				<a href="/acercade">acerda de</a>
-				<a href="/contacto">Contactos</a>
-				<a href="/categoria">Categoria</a>
+			<ul id="menu">
+
+			<li><a href="/">Inicio</a>
+			<li><a href="/acercade">acerda de</a>
+			<li><a href="/contacto">Contactos</a>
+			<li><a href="/categoria">Categoria</a>
 					<ul>
 						<li><a href="/EquiposMoviles">Equipos Moviles</a></li>
 						<li><a href="/Games">Games</a></li>
 						<li><a href="/MicroComputadoras">MicroComputadoras</a></li>
 						<li><a href="/Software">Softwares</a>
 					</ul>	
-			</nav>
+			</ul>
 		</div>
 	</header>
 
