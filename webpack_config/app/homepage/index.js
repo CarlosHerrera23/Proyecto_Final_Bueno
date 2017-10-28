05719e6a8d19eb2c441b61fb0b426b78f23f2ec8
Juan C. Herrera <1110788@ei.ing.edu.ni>
2017-10-28 12:24:01 -0600

    menu en page

--- webpack_config/app/homepage/index.js
+++ webpack_config/app/homepage/index.js
@@ -1,4 +1,65 @@
 
-const homepage = `<h1>Bienvenidos</h1> `
+const homepage = `
+
+	<main>
+		<section id="banner">
+			<img src="http://imaginadigital.mx/wp-content/uploads/2015/06/tienda-en-linea.jpg">
+			<div class="contenedor">
+				<h2>TechMarts La Tienda del Futuro</h2>
+				<p>Quieres Conocer mas Hacerca de La tienda</p>
+				<a href="#">Leer Mas</a>
+			</div>
+		</section>
+
+		<section id="bienvenido">
+			<h2>BIENVENIDO A NUESTRA TIENDA EN LINEA </h2>
+			<p>Somos una nueva tienda online. Ya sabemos que hay muchas, y seguramente muchas de ellas mejores que nosotros, pero estamos trabajando muy duro para llegar ahí. Somos un equipo joven y dinámico, a quienes nos encanta lo que hacemos. Intentamos dar a nuestros clientes el mejor servicio, y nos sentimos orgullosos de poco a poco haber ido fidelizado a varios de nuestros primeros clientes. Ofrecemos atención telefónica para cualquier duda, recomendaciones y si buscas algo que no tenemos, nosotros lo buscamos por ti, para aconsejarte donde encontrarlo si no te lo pudiéramos proporcionar nosotros. </p>
+			
+		</section>
+
+		<section id="blog">
+			<h3>Lo ultimo de Nuestra Tienda</h3>
+			<div class="contenedor">
+				<article>
+					
+					<img src="https://cdn.dribbble.com/users/300766/screenshots/1290406/imac.gif">
+					<h4>Micro Computadoras</h4>
+				</article>
+				<article>
+					
+					<img src="https://media.giphy.com/media/l1KVaBBR8VGg57Hdm/giphy.gif" >
+					<h4>Equipos Moviles</h4>
+				</article>
+				<article>
+					<img src="https://i.imgur.com/l0Jdr4I.gif">
+					<h4>Juegos</h4>
+				</article>
+
+			</div>
+		</section>
+
+		<section id="info">
+			<h3>Para Nosotros es importante brindarle Productos de calidad y Con garantia A nuestro cliente</h3>
+			<div class="contenedor">
+				<div class="info-pet">
+					<img src="http://ceoconsultores.mx/assets/images/certificacion-1400x881.png">
+					<h4>Certififcados</h4>
+				</div>
+				<div class="info-pet">
+					<img src="http://answare-tech.com/wp-content/uploads/2017/04/logo-iso-596x540.png">
+					<h4>Calidad</h4>
+				</div>
+				<div class="info-pet">
+					<img src="http://conceptodefinicion.de/wp-content/uploads/2011/09/garantia.png">
+					<h4>Garantia</h4>
+				</div>
+				<div class="info-pet">
+					<img src="http://www.myway.com.br/wp-content/uploads/2015/07/reembolso-proporcional-300x297.png">
+					<h4>Reembolsoo</h4>
+				</div>
+			</div>
+		</section>
+	</main>
+<h1>hola fgfg</h1> `
 
 export default homepage
