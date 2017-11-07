99be3ce007633e526c374242203446a254626d27
Juan C. Herrera <1110788@ei.ing.edu.ni>
2017-11-07 15:59:54 -0600

    Se modifico

--- webpack_config/app/categoria/index.js
+++ webpack_config/app/categoria/index.js
@@ -1,3 +1,36 @@
-const categoria = `<h1>Categoria</h1>`
+import firebase from 'firebase'
+import config from'./../config'
+
+if (!firebase.apps.length) {
+	firebase.initializeApp(config)
+}
+
+var db = firebase.database()   
+const categoria = () => {
+	const obtenerDatos= function (datos) {
+		var arrayDatos = datos.val()
+		//var html =  templateCat(arrayDatos)
+		var keys = Object.keys(arrayDatos)  
+
+		for (var i = 0; i < keys.length; i++) {
+			var key = keys[i]
+			var equipo = arrayDatos[key]
+			//'<h1>${equipo.Modelo}</h1>'
+
+      console.log(keys)
+       
+}
+
+		/*const main = document.querySelector('main')
+		main.innerHTML = index//layout(index)*/
+		
+
+	}
+
+	db.ref('Categoria').once('value').then(obtenerDatos)
+	
+	/*const main = document.querySelector('main')
+	main.innerHTML = layout(categoria)*/
+}
 
 export default categoria
