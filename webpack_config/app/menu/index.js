17e877240bebd77887ad78c72830a248b6fa1691
Juan C. Herrera <1110788@ei.ing.edu.ni>
2017-11-07 10:15:12 -0600

    Se limpio el codigo categoria

--- webpack_config/app/menu/index.js
+++ webpack_config/app/menu/index.js
@@ -2,46 +2,6 @@
 const menu = `
 
 <style type="text/css"> 
-#menu {
-margin: 0; 
-padding: 0; 
-} 
-#menu li { 
-display: inline; 
-} 
-#menu li a { 
-font-family: Arial; 
-font-size:15px; 
-text-decoration: none; 
-float:center; 
-padding: 10px; 
-background-color: none; 
-color: #fff; 
-width: 480px;
-margin: 0 auto;
-} 
-ul{
-	list-style:none
-}
-#menu li a:hover { 
-background-color: #2586d7; 
-margin-top:-7px; 
-padding-bottom:px;
-} 
-#menu li>a:hover{
-  color: black;
-  background-color: #eaeaea;
-}
-#menu>li{
-	text-align:center;
-	display: inline;
-}
-#menu>li>ul{
-  display: none;
-}
-#menu>li:hover>ul {
-  display:block;
-}
 </style>
 
 <html>
@@ -59,12 +19,7 @@ padding-bottom:px;
 			<li><a href="/acercade">acerda de</a>
 			<li><a href="/contacto">Contactos</a>
 			<li><a href="/categoria">Categoria</a>
-					<ul>
-						<li><a href="/EquiposMoviles">Equipos Moviles</a></li>
-						<li><a href="/Games">Games</a></li>
-						<li><a href="/MicroComputadoras">MicroComputadoras</a></li>
-						<li><a href="/Software">Softwares</a>
-					</ul>	
+						
 			</ul>
 		</div>
 	</header>
