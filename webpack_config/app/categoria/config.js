769c44b0202ceb45eb1dee3fa613363dc246bf65
Juan C. Herrera <1110788@ei.ing.edu.ni>
2017-11-07 16:14:42 -0600

    firebase

---
+++ webpack_config/app/categoria/config.js
@@ -0,0 +1,11 @@
+ var config = {
+    apiKey: "AIzaSyCFlaOycO9cjohYT88cKz6DnqGfmbUrJkE",
+    authDomain: "techstorefinal.firebaseapp.com",
+    databaseURL: "https://techstorefinal.firebaseio.com",
+    projectId: "techstorefinal",
+    storageBucket: "techstorefinal.appspot.com",
+    messagingSenderId: "216629161850"
+  }
+  
+
+  export default config
